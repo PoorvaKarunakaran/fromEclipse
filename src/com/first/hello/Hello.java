afac169ae33b9e6fbc1e7423fca2a8a595d61414
POORVA <poorva.k@accoliteindia.com>
2017-07-10 19:24:45 +0530

    added auto doc

--- src/com/first/hello/Hello.java
+++ src/com/first/hello/Hello.java
@@ -1,11 +1,26 @@
+/*
+ * 
+ */
 package com.first.hello;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Hello.
+ */
 public class Hello {
 
+	/**
+	 * Instantiates a new hello.
+	 */
 	public Hello() {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String hello = "helloworld";
